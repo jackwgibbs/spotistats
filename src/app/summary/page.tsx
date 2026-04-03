76634c606880c963191027d643d938b2e280bf12
Jack Gibbs <jackgibbs66@icloud.com>
2026-04-03 15:29:51 +0100

    Redirect to the summary page after successful login

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+
 export default function Summary() {
+    const { data: session } = useSession();
+
     return (
-        <div>Summary</div>
+        <>
+            <h1>Summary</h1>
+            <div className='p-6'>
+                <p className='text-white font-normal text-xl mt-5 mb-2'>Signed In as</p>
+                <span className='bold-txt'>{session?.user?.name}</span>
+                <p className='opacity-70 mt-8 mb-5 underline cursor-pointer' onClick={() => signOut()}>Sign Out</p>
+            </div>
+        </>
     )
 }
